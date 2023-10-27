2a8eb3f49a912702a3a5ceddc6508eabc4ce3657
MARVIN <o.silverygold@gmail.com>
2023-10-27 22:06:02 +0300

    Add registration and current user controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+ 
   devise_for :user, path: '', path_names: {
     sign_in: 'login',
     sign_out: 'logout',
@@ -8,4 +9,10 @@ Rails.application.routes.draw do
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
+  namespace :api do
+    namespace :v1 do
+      get 'current_user', to: 'current_user#index'
+    end
+  end
+
 end
