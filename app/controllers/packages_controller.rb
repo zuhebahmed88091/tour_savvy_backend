c594aea99811b8d2fa445ce4458c6d8f516d4243
MesakDuduCoder <duduaoa99@gmail.com>
2023-10-28 02:08:16 +0530

    Update controller

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -1,34 +1,34 @@
 class PackagesController < ApplicationController
-    def index
-        @packages = Package.all
-        render json: @packages
-    end
-    
-    def show
-        @package = Package.find(params[:id])
-        render json: @package
-    end
-    
-    def create
-        @package = Package.create(package_params)
-        render json: @package
-    end
-    
-    def update
-        @package = Package.find(params[:id])
-        @package.update(package_params)
-        render json: @package
-    end
-    
-    def destroy
-        @package = Package.find(params[:id])
-        @package.destroy
-        render json: @package
-    end
-    
-    private
-    
-    def package_params
-        params.require(:package).permit(:name, :description, :price, :image_url)
-    end
+  def index
+    @packages = Package.all
+    render json: @packages
+  end
+
+  def show
+    @package = Package.find(params[:id])
+    render json: @package
+  end
+
+  def create
+    @package = Package.create(package_params)
+    render json: @package
+  end
+
+  def update
+    @package = Package.find(params[:id])
+    @package.update(package_params)
+    render json: @package
+  end
+
+  def destroy
+    @package = Package.find(params[:id])
+    @package.destroy
+    render json: @package
+  end
+
+  private
+
+  def package_params
+    params.require(:package).permit(:name, :description, :price, :image_url)
+  end
 end
