b24933a5c3c3db341799e89e38e20c3c16b2f0e4
MARVIN <o.silverygold@gmail.com>
2023-11-02 21:16:14 +0300

    Add data to database - update seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
-user = User.create(username: "zuheb")
+user = User.create(username: "zuheb", email: "zuheb@email.com", password: "123456", admin: true)
+user = User.create(username: "marvin", email: "marvin@email.com", password: "123456")
 package = Package.create(name: "Mizoram", description: "Mizoram waterfall", image: "https://plus.unsplash.com/premium_photo-1675827055694-010aef2cf08f?auto=format&fit=crop&q=80&w=1424&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", package_type: [{ "name": "Golden","description": "3 day 4 nights", "price": 1000 }, { "name": "Silver","description": "2 day 3 nights", "price": 500 }, { "name": "Platinum","description": "7 days 7 nights", "price": 2000 }], user_id: user.id)
 reservation = Reservation.create(reservation_date: "2021-10-28", city_name: "Mizoram", package_name: "Mizoram" , package_type: "Golden", user_id: user.id)
 package.reservations << reservation
