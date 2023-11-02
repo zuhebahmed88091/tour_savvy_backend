b3958067d558e8ea65f4d4509ab6f9b6a58c1a97
MARVIN <o.silverygold@gmail.com>
2023-11-02 21:17:14 +0300

    update packages controller

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -3,10 +3,12 @@ class PackagesController < ApplicationController
     @packages = Package.all
     render json: @packages
   end
+
   def show
     @package = Package.find(params[:id])
     render json: @package
   end
+
   def create
     @package = Package.new(package_params)
     authorize! :create, @package
@@ -17,23 +19,29 @@ class PackagesController < ApplicationController
       render json: { errors: @package.errors.full_messages }, status: :unprocessable_entity
     end
   end
+
   def update
     @package = Package.find(params[:id])
     @package.update(package_params)
     render json: @package
   end
+
   def destroy
     @package = Package.find(params[:id])
     authorize! :destroy, @package
+
     if @package.reservations.any?
       @package.reservations.each do |reservation|
         reservation.packages.delete(@package)
       end
     end
+
     @package.destroy
     render json: @package
   end
+
   private
+  
   def package_params
     params.require(:package).permit(:name, :description, :price, :image, package_type: %i[name description price])
   end
