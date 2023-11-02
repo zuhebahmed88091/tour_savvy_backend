4cc30e2480888e0820c81b46e6ca85fa4ea5d56c
Zuheb Ahmed <zuhebahmed88091@gmail.com>
2023-11-03 00:21:12 +0600

    Add ability.rb for authorization

---
+++ app/models/ability.rb
@@ -0,0 +1,13 @@
+class Ability
+    include CanCan::Ability
+
+    def initialize(user)
+      user ||= User.new
+
+      return unless user.admin?
+
+      can :manage, :all
+      cannot :index, Reservation
+    end
+
+  end
