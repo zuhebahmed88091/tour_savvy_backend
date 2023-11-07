a4efa2718add9326faf92b7ae0e686927e38219a
MesakDuduCoder <duduaoa99@gmail.com>
2023-11-07 15:27:02 +0530

    Add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_many :reservations, dependent: :destroy
   has_many :packages, dependent: :destroy
   validates :username, uniqueness: true, presence: true
+  validates :email, uniqueness: true, presence: true
+  validates :password, presence: true
+  validates :password, length: { minimum: 6 }
 
   def admin?
     admin
